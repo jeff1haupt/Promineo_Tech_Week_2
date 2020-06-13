d345af9aab2c809c670ace30eaafad51dcd37732
jeff1haupt <jhaupt@southbendin.gov>
2020-06-13 18:25:06 -0400

    Updated finished App.java file for Week 2

--- App.java
+++ App.java
@@ -6,7 +6,7 @@ public class App {
 
 		
 		boolean isHotOutside = false;
-		boolean isWeekday = false; 
+		boolean isWeekDay = false; 
 		boolean hasMoneyInPocket = true; 
 		
 		double costOfMilk = 2.09;
@@ -14,9 +14,16 @@ public class App {
 		int thirstLevel = 3; 
 		
 		boolean shouldBuyIcecream = isHotOutside && hasMoneyInPocket;
-		
+		boolean willGoSwimming = isHotOutside && isWeekDay;
+		boolean isAGoodDay = isHotOutside && hasMoneyInPocket && isWeekDay;
+		boolean willBuyMilk = isHotOutside && (thirstLevel >= 3) && (moneyInWallet >= costOfMilk*2);
+
 		
 		System.out.println("Should I buy ice cream? " + shouldBuyIcecream);
+		System.out.println("Should I go swimming? " + willGoSwimming);
+		System.out.println("Is it a good day? " + isAGoodDay);
+		System.out.println("Should I buy milk? " + willBuyMilk);
+
 		
 		
 	}
