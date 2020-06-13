e2885546f5aeabbd3ea75af42f97713e7e7aab56
jeff1haupt <jhaupt@southbendin.gov>
2020-06-13 18:13:23 -0400

    Updated App.java file

--- App.java
+++ App.java
@@ -5,7 +5,19 @@ public class App {
 		// TODO Auto-generated method stub
 
 		
-		System.out.println("Hello".charAt(0)=='h');
+		boolean isHotOutside = false;
+		boolean isWeekday = false; 
+		boolean hasMoneyInPocket = true; 
+		
+		double costOfMilk = 2.09;
+		double moneyInWallet = 50;
+		int thirstLevel = 3; 
+		
+		boolean shouldBuyIcecream = isHotOutside && hasMoneyInPocket;
+		
+		
+		System.out.println("Should I buy ice cream? " + shouldBuyIcecream);
+		
 		
 	}
 
